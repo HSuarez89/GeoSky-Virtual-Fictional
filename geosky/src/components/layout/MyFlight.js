7732388ec3855e7509e4577c2af2cb4da2d614a8
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2024-01-11 10:55:33 -0300

    Corrigindo seção MyFlight

--- geosky/src/components/layout/MyFlight.js
+++ geosky/src/components/layout/MyFlight.js
@@ -1,112 +1,58 @@
 import PilotCard from "./PilotCard"
 import styles from "./MyFlight.module.css"
 import {useState, useEffect} from "react"
-import Options from "../options/Options"
+
 
 function MyFlight(){
-    const [pilot, setPilot] = useState([])
-    const [airport, setAirport] = useState([])
-    const [airportId, setAirportId] = useState([])
-    const [selectedDestination, setSelectedDestination] = useState('')
-    const [origin, setOrigin] = useState([])
-    const [destination, setDestination] = useState({})
+
+    const [pilot,setPilot] = useState([])
+    const [locationId, setLocationId] = useState([])
+    const [pilotLocation, setPilotLocation] = useState([])
+    const [airportName, setAirportName] = useState([])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/pilot`, {
+        fetch('http://localhost:5000/pilot', {
             method: "GET",
-            headers: {"Content-Type": 'application/json'},
-        })
-        .then((resp) => resp.json())
+            headers: {'Content-Type': 'application/json'}
+        }).then((resp) => resp.json())
         .then((data) => {
             setPilot(data)
         })
         .catch((err) => console.log(err))
-        },[])
+    }, [])
 
     useEffect(() => {
         if(pilot.length > 0){
-            setAirportId(pilot[0].location)
+            setLocationId(pilot[0].location)
         }
-    },[airportId, pilot])
+    },[pilot])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/airports/${airportId}`, {
-            method: "GET",
-            headers: {'Content-Type': 'application/json'},
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setAirport(data.city)
-            setOrigin(data)
-        })
-        .catch((err) => console.log(err))
-    },[airportId])
-
-    const handleDestinationSelect = (destination) => {
-        setSelectedDestination(destination)
-    }
-
-    function bidFlight(){
-        fetch(`http://localhost:5000/airports/`, {
+        fetch(`http://localhost:5000/airports/${locationId}`,{
             method: "GET",
             headers: {'Content-Type': 'application/json'}
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-                setDestination(data)
-                const destinationExists = data.find(item => item.name === selectedDestination)
-                if(destinationExists){
-                    setDestination(destinationExists)
-                }
-        })
-        .catch((err) => console.log(err))
-
-        const bidContent = {
-            origin: origin,
-            destination: destination
-        }
-
-        fetch('http://localhost:5000/bids', {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(bidContent)
         }).then((resp) => resp.json())
         .then((data) => {
-            console.log(data)
+            setPilotLocation(data)
+            setAirportName(data.city)
         })
-        .catch((err) => console.log(err))
-    }
-
-    
+    },[locationId])
 
     return(
-        <div className={styles.div_container}>
+        <div className={styles.div_container} >
             <div className={styles.text}>
                 <h1>My Flight</h1>
-                <p>Here, you can see your current information.</p>
-            </div>
-            <div className={styles.card_container}>
-                {pilot.map((pilot, index) =>
-                    <PilotCard
-                    key={index}
-                    name={pilot.name}
-                    location={airport}
-                    hours={pilot.hours}
-                    />
-                )}
-            </div>
-            <div className={styles.text}>
-                <h1>Next Flight</h1>
-                <p>Here you can bid your next flight:</p>
-                <p><span>From: </span>{airport}</p>
-                <div className={styles.select_container}>
-                    <p><span>To: </span></p>
-                    <Options originLocation={airportId} onSelect={handleDestinationSelect}/>
-                    <button onClick={bidFlight}>Bid</button>
+                <p>You can bid your flights here!</p>
+                <div className={styles.card_container}>
+                    {pilot.map((pilot, index) =>
+                        <PilotCard
+                            key={index}
+                            name={pilot.name}
+                            location={airportName}
+                            flights={pilot.flights}
+                        />
+                    )}
                 </div>
-            </div>
-            <div className={styles.text}>
-                <h1>Log</h1>
             </div>
         </div>
     )
--- geosky/src/components/layout/PilotCard.js
+++ geosky/src/components/layout/PilotCard.js
@@ -1,14 +1,14 @@
 import profilepic from "../../img/Perfil.jpg"
 import styles from "./PilotCard.module.css"
 
-function PilotCard({name, hours, location, handleReset}){
+function PilotCard({name, flights, location, handleReset}){
 
 
     return(
         <div className={styles.pilot_card}>
             <img src={profilepic} alt="Profile"></img>
             <p><span>Name:</span> {name}</p>
-            <p><span>Flight Hours:</span> {hours}</p>
+            <p><span>Flights:</span> {flights}</p>
             <p><span>Location:</span> {location}</p>
         </div>
     )
